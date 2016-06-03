668d82d8119dd54cc9cfb2414696d0888f83a7f3
kanon123 <398920390@qq.com>
2016-06-03 13:45:10 +0800

    udpate1

--- src/kanon/test/Test.java
+++ src/kanon/test/Test.java
@@ -12,5 +12,6 @@ public class Test
 	ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring.xml");
 	User user = (User) ctx.getBean("user");
 	user.sayInfo();
+	System.out.println("-----------");
     }
 }
